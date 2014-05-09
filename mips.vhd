fa699e61c4e17ad1f0b63224e6b0091f7f32964e
unknown <C16Eric.Wardner@C16EJWardner.usafa.ds.af.edu>
2014-05-09 07:10:48 -0600

    syntax works

--- mips.vhd
+++ mips.vhd
@@ -130,7 +130,7 @@ library IEEE; use IEEE.STD_LOGIC_1164.all;
 entity mux3 is -- three-input multiplexer
   generic(width: integer);
   port(d0, d1, d2: in  STD_LOGIC_VECTOR(width-1 downto 0);
-       s:      in  STD_LOGIC;
+       s:      in  STD_LOGIC_VECTOR(1 downto 0);
        y:      out STD_LOGIC_VECTOR(width-1 downto 0));
 end;
 ---------------------------------------------------------
@@ -389,7 +389,7 @@ end;
 
 architecture behave of mux3 is
 begin
-  y <= d0 when s = "01" else
-  y <= d1 when s = "10" else
-  y <= d1;
+  y <= d0 when s = "00" else
+		 d1 when s = "01" else
+		 d2 when s = "10";
 end;
